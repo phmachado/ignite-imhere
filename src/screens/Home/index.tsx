8c071a62d9a2abed6251bbdec34b5cdb35e93a1b
Pedro Machado <pedro.machado@liferay.com>
2022-08-09 16:39:46 -0300

    Project 01 | Input and add button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,23 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
 export default function Home() {
+  function handleParticipantAdd() {
+    console.log("Clique");
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.eventName}>Nome do Evento</Text>
+      <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6B6B6B"
+      />
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 }
