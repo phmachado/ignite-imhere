3f18ca3ab0140d58c47c6f890f4634070ae2fd2e
Pedro Machado <pedro.machado@liferay.com>
2022-08-09 11:39:57 -0300

    Project 01 | Using StyleSheet

--- App.tsx
+++ App.tsx
@@ -1,27 +1,25 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "#131016",
-        padding: 24,
-      }}
-    >
-      <Text
-        style={{
-          color: "#FDFCFE",
-          fontSize: 24,
-          fontWeight: "bold",
-          marginTop: 48,
-        }}
-      >
-        Nome do Evento
-      </Text>
-      <Text style={{ color: "#6B6B6B", fontSize: 16 }}>
-        Sexta, 4 de Novembro de 2022.
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do Evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#131016",
+    padding: 24,
+  },
+  eventName: {
+    color: "#FDFCFE",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+  eventDate: { color: "#6B6B6B", fontSize: 16 },
+});
